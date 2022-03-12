b844b2442a386d22a4f253bbe85e6de91bf46fed
David Bucher <d.bucher97@gmail.com>
2022-03-13 00:30:47 +0100

    All features in one coding session

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Obsidian does not offer a good integration for fleeting notes. Thats where this
 plugin aims to step in.
 
-#### What are fleeting notes?
+### What are fleeting notes?
 
 Fleeting notes are the quick notes you take whenever a idea pops into mind, you
 have a discussion and write down your notes, etc. All the notes that are perfect
@@ -14,7 +14,7 @@ need to be worked over and integrated into existing permanent notes or
 eventually creating new permanent notes. This should happen in quick succession
 to the taking of the fleeting note, ideally the same day.
 
-#### What tries this plugin to solve?
+### What tries this plugin to solve?
 
 - I want my fleeting notes to have no intentional file name, i.e. randomly
   generated.
@@ -24,3 +24,14 @@ to the taking of the fleeting note, ideally the same day.
   they are not processed.
 - I want to be able to clean fleeting notes when they are processed or not
   worked on for period of time.
+
+#### Why do I want random file names?
+
+Often when faced with quickly writing down an idea, you get blocked by first
+thinking about a title for that specific note. For the purpose of fleeting notes
+it is much more important to instantly write down your brain process and not be
+hindered by thinking of a title that encapsulates the thought. The latter is
+better thought of after writing the note. You can rename the file, but just
+giving it a `h1` title should be sufficient. Also make sure to pack your title
+to be able to identify the note later.
+
--- main.ts
+++ main.ts
@@ -1,137 +1,235 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, TFile, TFolder, Notice, FuzzySuggestModal, Plugin, PluginSettingTab, Setting, Vault, Modal } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FleetingNotesSettings {
+    path: string;
+    len: number;
+    maxDays: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FleetingNotesSettings = {
+    path: 'fleeting',
+    len: 6,
+    maxDays: 30,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FleetingNotes extends Plugin {
+    settings: FleetingNotesSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        const ribbonIconEl = this.addRibbonIcon('paper-plane', 'New Fleeting Note', (evt: MouseEvent) => {
+            this.createNewFleeting();
+        });
+        ribbonIconEl.addClass('fleeting-note-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        this.addCommand({
+            id: 'new-fleeting-note',
+            name: 'New fleeting note',
+            callback: () => {
+                this.createNewFleeting();
+            }
+        });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        this.addCommand({
+            id: 'list-fleeting-notes',
+            name: 'List fleeting notes',
+            callback: () => {
+                new FleetingModal(this.app, this.settings.path).open();
+            }
+        });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        this.addCommand({
+            id: 'clean-fleeting-notes',
+            name: 'Clean fleeting notes',
+            callback: () => {
+                this.cleanFleetings();
+            }
+        });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        this.addSettingTab(new FleetingNotesSettingsTab(this.app, this));
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    onunload() {
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    }
 
-	onunload() {
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    newID(): string {
+        return Math.random().toString(36).substr(2, this.settings.len);
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async createNewFleeting(): Promise<void> {
+        let name = this.newID();
+        const fileName = `${this.settings.path}/${name}.md`;
+        try {
+            while (await this.app.vault.adapter.exists(fileName)) {
+                name = this.newID();
+            }
+            const file = await this.app.vault.create(fileName, "# Fleeting");
+            this.app.workspace.activeLeaf.openFile(file);
+        } catch (error) {
+            new Notice(error.toString());
+        }
+    }
+
+    cleanFleetings(): void {
+        const folder = this.app.vault.getAbstractFileByPath(this.settings.path) as TFolder;
+        const todelete = folder.children.filter(file => {
+            // rigourously delete folders, fleeting dir should be flat.
+            if (file instanceof TFolder) {
+                return true;
+            }
+            const tfile = file as TFile;
+            const dms = new Date().getTime() - tfile.stat.mtime
+            if (dms > this.settings.maxDays * 24 * 3600 * 1000) {
+                return true;
+            }
+            const cache = this.app.metadataCache.getFileCache(tfile);
+            if (cache && 'tags' in cache) {
+                return cache.tags.map(({tag}) => tag).contains("#processed");
+            }
+            return false;
+        });
+        if(todelete.length == 0) return;
+        new ConfirmModal(this.app, () => {
+            todelete.forEach(element => {
+                this.app.vault.delete(element, true);
+            });
+        },todelete.length).open();
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ConfirmModal extends Modal {
+    files: number;
+    onSubmit: () => void;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, onSubmit: () => void, files: number) {
+        super(app);
+        this.onSubmit = onSubmit;
+        this.files = files;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    onOpen() {
+        let { contentEl } = this;
+        contentEl.setText(`Do you whish to delete ${this.files} fleeting notes?`); 
+        new Setting(contentEl).addButton(button => button
+            .setButtonText("Yes").setCta().onClick(() => {
+            this.close();
+            this.onSubmit();
+        }));
+    }
+
+    onClose() {
+        let { contentEl } = this;
+        contentEl.empty();
+    }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class FleetingModal extends FuzzySuggestModal<TFile> {
+    fleets: TFile[];
+    path: string;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, path: string) {
+        super(app);
+        this.path = path;
+        this.init();
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    init() {
+        try {
+            const folder = this.app.vault.getAbstractFileByPath(this.path) as TFolder;
+            this.fleets = folder.children.filter(afile => afile instanceof TFile) as TFile[];
+            this.fleets = this.fleets.filter(file => {
+                const cache = this.app.metadataCache.getFileCache(file);
+                if(cache && 'tags' in cache) {
+                    return !cache.tags.map(({tag}) => tag).contains('#processed');
+                } else {
+                    return true;
+                }
+            });
+            this.fleets.sort(file => file.stat.mtime);
+        } catch (error) {
+            new Notice(error.toString());
+        }
+    }
 
-		containerEl.empty();
+    getItems(): TFile[] {
+        return this.fleets;
+    }
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+    getItemText(item: TFile): string {
+        const cache  = this.app.metadataCache.getFileCache(item);
+        if(cache && 'headings' in cache) {
+            try {
+                return cache.headings.filter(({level}) => level == 1)[0].heading;
+            } catch (_) { }
+        }
+        return item.name;
+    }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+    onChooseItem(item: TFile): void {
+        this.app.workspace.activeLeaf.openFile(item);
+    }
+
+}
+
+
+class FleetingNotesSettingsTab extends PluginSettingTab {
+    plugin: FleetingNotes;
+
+    constructor(app: App, plugin: FleetingNotes) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const { containerEl } = this;
+
+        containerEl.empty();
+
+        new Setting(containerEl)
+            .setName('Fleeting Notes Directory')
+            .setDesc('Path to the Directory for fleeting notes')
+            .addText(text => text
+                .setPlaceholder('dir')
+                .setValue(this.plugin.settings.path)
+                .onChange(async (value) => {
+                    this.plugin.settings.path = value;
+                    await this.plugin.saveSettings();
+                }));
+
+        new Setting(containerEl)
+            .setName('ID Lenght')
+            .setDesc('Lenght of random ID')
+            .addSlider(slider => slider
+                .setLimits(4, 11, 1)
+                .setValue(this.plugin.settings.len)
+                .onChange(async (value) => {
+                    this.plugin.settings.len = value;
+                    await this.plugin.saveSettings();
+                }).setDynamicTooltip());
+
+        new Setting(containerEl)
+            .setName('Fleeting Notes Lifetime')
+            .setDesc('Number of days a fleeting note should not be deleted')
+            .addText(text => text
+                .setPlaceholder('days')
+                .setValue(`${this.plugin.settings.maxDays}`)
+                .onChange(async (value) => {
+                    const val = parseInt(value);
+                    if(!isNaN(val) && val > 0) {
+                        this.plugin.settings.maxDays = val;
+                    }
+                    await this.plugin.saveSettings();
+                }));
+
 	}
 }
